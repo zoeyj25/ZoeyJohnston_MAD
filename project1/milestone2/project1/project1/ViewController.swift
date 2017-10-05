7863e6f57060de7bf4e1f4719d7632b648e690a4
zoeyj25 <31741147+zoeyj25@users.noreply.github.com>
2017-10-04 22:18:47 -0600

    Add files via upload

---
+++ project1/milestone2/project1/project1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by Zoey Johnston on 10/2/17.
+//  Copyright © 2017 Zoey Johnston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
